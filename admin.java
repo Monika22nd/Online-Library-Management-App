10b4af6ebd4c465fd4e99dd4601681c3cf61edf6
khanhhsd <67154851+khanhhsd@users.noreply.github.com>
2025-09-29 21:31:53 +0700

    Create admin.java

---
+++ admin.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class Admin {
+    // Attributes
+    private String username = "admin";
+    private String password = "1";
+    private String email = "admin@gmail.com";
+    private String phone = "0987654321";
+  
+    //Create user
+    public void createUser(Person user) {
+        
+    }
+    // Delete user
+    public void deleteUser(String userId) {
+        
+    }
+    //Update user
+    public void updateUser(Person user) {
+        
+    }
+    //Reset password
+    public void resetPassword(String userId) {
+       
+    }
+    //Add book
+    public void addBook(Book book) {
+        
+    }
+    //Remove book
+    public void removeBook(String isbn) {
+        
+    }
+    //Update Book
+    public void updateBook(Book book) {
+        
+    }
+}
